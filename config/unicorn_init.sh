01c74fdb72d957a72ca26d826ed88346161a1fe6
arlyxiao <kingla_pei@163.com>
2015-01-10 10:34:14 +0800

    fix unicorn init

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -1,71 +1,113 @@
 #!/bin/sh
+#
+# init.d script for single or multiple unicorn installations. Expects at least one .conf 
+# file in /etc/unicorn
+#
+# Modified by jay@gooby.org http://github.com/jaygooby
+# based on http://gist.github.com/308216 by http://github.com/mguterl
+#
+## A sample /etc/unicorn/my_app.conf
+## 
+## RAILS_ENV=production
+## RAILS_ROOT=/var/apps/www/my_app/current
+#
+# This configures a unicorn master for your app at /var/apps/www/my_app/current running in
+# production mode. It will read config/unicorn.rb for further set up. 
+#
+# You should ensure different ports or sockets are set in each config/unicorn.rb if
+# you are running more than one master concurrently.
+#
+# If you call this script without any config parameters, it will attempt to run the
+# init command for all your unicorn configurations listed in /etc/unicorn/*.conf
+#
+# /etc/init.d/unicorn start # starts all unicorns
+#
+# If you specify a particular config, it will only operate on that one
+#
+# /etc/init.d/unicorn start /etc/unicorn/my_app.conf
+
+RAILS_ENV=production
+RAILS_ROOT=/var/www/io_store
+
 set -e
-# Example init script, this can be used with nginx, too,
-# since nginx and unicorn accept the same signals
-
-# Feel free to change any of the following variables for your app:
-TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/var/www/io_store
-PID=$APP_ROOT/var/www/io_store/pids/unicorn.pid
-CMD="$APP_ROOT/bin/unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
-action="$1"
-set -u
-
-old_pid="$PID.oldbin"
-
-cd $APP_ROOT || exit 1
 
 sig () {
-        test -s "$PID" && kill -$1 `cat $PID`
+  test -s "$PID" && kill -$1 `cat "$PID"`
 }
 
 oldsig () {
-        test -s $old_pid && kill -$1 `cat $old_pid`
+  test -s "$OLD_PID" && kill -$1 `cat "$OLD_PID"`
 }
 
-case $action in
-start)
-        sig 0 && echo >&2 "Already running" && exit 0
-        su -c "$CMD" - <username>
-        ;;
-stop)
-        sig QUIT && exit 0
-        echo >&2 "Not running"
-        ;;
-force-stop)
-        sig TERM && exit 0
-        echo >&2 "Not running"
-        ;;
-restart|reload)
-        sig HUP && echo reloaded OK && exit 0
-        echo >&2 "Couldn't reload, starting '$CMD' instead"
-        su -c "$CMD" - <username>
-        ;;
-upgrade)
-        if sig USR2 && sleep 2 && sig 0 && oldsig QUIT
-        then
-                n=$TIMEOUT
-                while test -s $old_pid && test $n -ge 0
-                do
-                        printf '.' && sleep 1 && n=$(( $n - 1 ))
-                done
-                echo
+cmd () {
 
-                if test $n -lt 0 && test -s $old_pid
-                then
-                        echo >&2 "$old_pid still exists after $TIMEOUT seconds"
-                        exit 1
-                fi
-                exit 0
-        fi
-        echo >&2 "Couldn't upgrade, starting '$CMD' instead"
-        su -c "$CMD" - <username>
-        ;;
-reopen-logs)
-        sig USR1
-        ;;
-*)
-        echo >&2 "Usage: $0 "
-        exit 1
-        ;;
-esac
+  case $1 in
+    start)
+      sig 0 && echo >&2 "Already running" && exit 0
+      echo "Starting"
+      $CMD
+      ;;  
+    stop)
+      sig QUIT && echo "Stopping" && exit 0
+      echo >&2 "Not running"
+      ;;  
+    force-stop)
+      sig TERM && echo "Forcing a stop" && exit 0
+      echo >&2 "Not running"
+      ;;  
+    restart|reload)
+      sig USR2 && sleep 5 && oldsig QUIT && echo "Killing old master" `cat $OLD_PID` && exit 0
+      echo >&2 "Couldn't reload, starting '$CMD' instead"
+      $CMD
+      ;;  
+    upgrade)
+      sig USR2 && echo Upgraded && exit 0
+      echo >&2 "Couldn't upgrade, starting '$CMD' instead"
+      $CMD
+      ;;  
+    rotate)
+            sig USR1 && echo rotated logs OK && exit 0
+            echo >&2 "Couldn't rotate logs" && exit 1
+            ;;  
+    *)  
+      echo >&2 "Usage: $0 <start|stop|restart|upgrade|rotate|force-stop>"
+      exit 1
+      ;;  
+    esac
+}
+
+setup () {
+
+  echo -n "$RAILS_ROOT: "  
+  cd $RAILS_ROOT || exit 1
+  export PID=$RAILS_ROOT/tmp/pids/unicorn.pid
+  export OLD_PID="$PID.oldbin"
+
+  CMD="/usr/bin/unicorn_rails -c config/unicorn.rb -E $RAILS_ENV -D"
+}
+
+start_stop () {
+  
+  # either run the start/stop/reload/etc command for every config under /etc/unicorn
+  # or just do it for a specific one
+
+  # $1 contains the start/stop/etc command
+  # $2 if it exists, should be the specific config we want to act on
+  if [ $2 ]; then
+    . $2
+    setup
+    cmd $1
+  else
+    for CONFIG in /etc/unicorn/*.conf; do
+      # import the variables
+      . $CONFIG
+      setup
+
+      # run the start/stop/etc command
+      cmd $1
+    done
+   fi
+}
+
+ARGS="$1 $2"
+start_stop $ARGS
